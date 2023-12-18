cbff3e3e0cd3123884c4a0963a4d6e0acc6c75a7
Marcin <wanago.marcin@gmail.com>
2023-12-18 01:52:16 +0100

    test(Counter): remove one unnecessary test

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Counter } from '.';
 import { describe, expect, it } from 'vitest';
 
@@ -8,21 +8,6 @@ describe('The Counter component', () => {
       const counter = render(<Counter />);
 
       const paragraph = counter.getByText('The number of clicks: 0', {
-        selector: 'p',
-      });
-
-      expect(paragraph).toBeDefined();
-    });
-  });
-  describe('if the button is clicked once', () => {
-    it('should display 1', async () => {
-      const counter = render(<Counter />);
-
-      const button = counter.getByText('Click', { selector: 'button' });
-
-      fireEvent.click(button);
-
-      const paragraph = await counter.findByText('The number of clicks: 1', {
         selector: 'p',
       });
 
